28dc47f970ca9e8b78a3c53c009ad7f53f9f2547
akash23-ai <akashbhardwaj2024@gmail.com>
2024-03-02 11:50:38 +0530

    Updated components

---
+++ README.md
@@ -0,0 +1,20 @@
+# Chat App
+
+## This is A chat Applicatio Made by using 
+
+- Socket.io
+- Next JS
+- Express
+- Next Router
+- Tailwind CSS
+- MONGODB 
+- ShadCn
+
+
+
+# Home Page
+
+![Home](/urlshortnerclient//public/profile.png)
+
+## Error Handling
+This Application Also has Support for Error Handling 
---
+++ urlshortnerclient/src/pages/components/error.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Button } from '@/components/ui/button';
+
+function Error({handleErrorButton}: {handleErrorButton: () => void}) {
+
+    
+    return (
+        <div className="flex flex-col mt-6 items-center h-screen">
+          <h1 className="text-7xl mb-14">Error</h1>
+          {/* <button
+            className="p-4 bg-green-600 rounded-md text-xl font-semibold"
+            onClick={handleErrorButton}
+          >
+            Go to Url Page
+          </button> */}
+          <Button variant={"destructive"} size={"lg"} className='text-xl font-semibold' onClick={handleErrorButton}> Go to Home Page</Button>
+        </div>
+      );
+}
+
+export default Error
