a51198c926f63c077b53a96d36c6a1ab48ee3936
akash23-ai <akashbhardwaj2024@gmail.com>
2024-05-04 21:38:38 +0530

    Addde signin page

--- urlshortnerclient/src/pages/components/appbar.tsx
+++ urlshortnerclient/src/pages/components/appbar.tsx
@@ -7,7 +7,7 @@ export default function AppBar(){
             <Link href={""} className="text-lg font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#1F89DB] via-[#F42A8B] to-[#1F89DB]">URL Shortner</Link>
             <ul id="nav-mobile" className="flex gap-6 text-lg font-semibold">
                 <li className=""><Link href="/">Home</Link></li>
-                <li><Link href="/login">Login</Link></li>
+                <li><Link href="/signin">Login</Link></li>
                 <li><Link href="/register">Register</Link></li>
             </ul>
         </div>
