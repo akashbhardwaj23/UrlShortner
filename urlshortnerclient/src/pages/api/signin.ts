0e164c709631a1397b2e7c054a8fee8f28952060
akash23-ai <akashbhardwaj2024@gmail.com>
2024-05-08 09:41:34 +0530

    Added prisma

--- urlshortnerclient/src/pages/api/signin.ts
+++ urlshortnerclient/src/pages/api/signin.ts
@@ -1,15 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from "@/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
     const stringData = req.body;
     const data = JSON.parse(stringData);
-    return res.status(200).json({ name: data.username });
+
+    const user = await prisma.user.create({
+        data : {
+          email : data.username,
+          name : "nAME",
+          password : data.password
+        }
+    })
+
+    console.log("User is Created")
+
+    return res.status(200).json({ user: user } as unknown as Data);
 }
